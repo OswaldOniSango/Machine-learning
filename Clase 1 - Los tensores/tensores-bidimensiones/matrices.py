7a5955d36bb28a8645959840da66607bf454e73b
Oswaldo Hernandez <oswaldo.hernandez@insite360suite.com>
2026-01-30 14:19:30 -0300

    fin de tensores

--- Clase 1 - Los tensores/tensores-bidimensiones/matrices.py
+++ Clase 1 - Los tensores/tensores-bidimensiones/matrices.py
@@ -8,3 +8,13 @@
 #en caso de referirte a toda la columna 2 seria x:,2
 #en caso de referirte a toda la file 2 seria x2,:
 
+#Notaacion tensorial generica
+#Mayusculas negrita cursiva sin serifa 
+#tensor de 4 dimensiones seri X[i,j,k,l]
+
+#Tensores de rango superiod
+#Son comunes par alas imagenes donde cada dimension corresponde a
+#1.-Numero de imagenes en el lote de entrenamiento, por ejemplo 32
+#2.-Altura de la imagen en pixeles
+#3.-Anchira d ela imagen en pizeles, 
+#4.-Numero de canales de color, por hemplo, 3 para imagenes a todo color (r,g,b)
---
+++ Clase 1 - Los tensores/tensores-bidimensiones/tensor-rango-superior.py
@@ -0,0 +1,10 @@
+import torch as pt
+import tensorflow as tf
+
+image_pt = pt.zeros([1,28,28,3])
+#print(image_pt)
+
+
+image_tf = tf.zeros([32,28,28,3])
+
+
